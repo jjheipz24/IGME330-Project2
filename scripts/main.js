a9332eb2fee7c63f0f9f0ca875f5607a91ad4dd3
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2019-04-14 17:16:16 -0400

    Gets isbn number from book and uses it to get cover image link

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,8 @@ let app = new Vue({
         bookHeader: " ",
         bookAuthor: " ",
         bookDescrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        bookImgLink: "http://covers.openlibrary.org/b/isbn/",
+        bookISBN: "",
         bookPic: "../bookpic.jpg",
         numResults: ""
     },
@@ -40,10 +42,19 @@ let app = new Vue({
                         //isolates first result found for book and all of its properties
                         json = json.docs[0];
                         this.contents = json; //sets contents to docs array in JSON file
-                        this.bookAuthor = this.contents.author_name[0]; //sets bookAuthor to the item in [0] of author_names array
+                        this.setContents();
                     })
             }
-        } // end search
+        }, // end search
+
+        setContents() {
+            this.bookAuthor = this.contents.author_name[0]; //sets bookAuthor to the item in [0] of author_names array
+            this.bookISBN = this.contents.isbn[0]; //sets isbn of book
+            this.bookImgLink += this.bookISBN;
+            this.bookImgLink += `-M.jpg`;
+            this.bookPic = this.bookImgLink;
+
+        }
 
     } // end methods
 
