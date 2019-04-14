18177e43bf10dbd17888791685480715b536ada5
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2019-04-14 16:39:09 -0400

    retrieves author info of query search and sets it in bookAuthor variable

--- scripts/main.js
+++ scripts/main.js
@@ -2,30 +2,30 @@ let app = new Vue({
     el: '#root',
     data: {
         link: "https://cors-anywhere.herokuapp.com/http://openlibrary.org/search.json?title=",
-        list: {
+        contents: {
 
         },
         bookName: " ",
         bookHeader: " ",
-        bookAuthor: "Stephen King",
+        bookAuthor: " ",
         bookDescrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         bookPic: "../bookpic.jpg",
         numResults: ""
     },
     methods: {
         search() {
-            //if (! this.term.trim()) return;
-            //            if (this.bookName == " " || this.numResults == "") {
+
+            //Won't search if nothing added in search bar
             if (this.bookName == " ") {
                 return;
             } else {
-                this.bookHeader = this.bookName;
+                this.bookHeader = this.bookName; //sets book header to original query
+                //Changes input to acceptable form to be searched
                 this.bookName = this.bookName.toLowerCase();
                 this.bookName = this.bookName.replace(/ /g, "+");
                 this.link += `${this.bookName}`;
-                //                        console.log(this.bookName);
-                //                        console.log(this.link);
-                this.bookName = "";
+
+                this.bookName = ""; //clears search bar
                 fetch(this.link)
                     .then(response => {
                         if (!response.ok) {
@@ -36,8 +36,11 @@ let app = new Vue({
                     })
                     .then(json => {
                         console.log(json);
-                        json = json.docs[0]
-                        this.list = json;
+
+                        //isolates first result found for book and all of its properties
+                        json = json.docs[0];
+                        this.contents = json; //sets contents to docs array in JSON file
+                        this.bookAuthor = this.contents.author_name[0]; //sets bookAuthor to the item in [0] of author_names array
                     })
             }
         } // end search
