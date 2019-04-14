826f050c19246b4e8c4eede46c601098528af68d
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2019-04-14 15:36:24 -0400

    empty spaces

--- scripts/components.js
+++ scripts/components.js
@@ -8,7 +8,7 @@ Vue.component('book-title', {
 
 Vue.component('book-info', {
     props: ['author', 'descrip'],
-    template: `        <div class="info">
+    template: `<div class="info">
         <h3>{{author}}</h3>
         <p>{{descrip}}</p>
         </div>`
