6cbc0e16e74b09bc1278afa6c6dfd5273ef83d43
jjheipz24 <heipler.jj@gmail.com>
2019-05-02 01:38:29 -0400

    filters searches and adds count for each search

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,6 +1,6 @@
-
-//array to hold all searches
-let allQueries = [];
+let allQueries = []; //array to hold all searches
+let counts = {}; //object to add searches and counter
+let filteredSearches = []; //filters
 
 // Initialize Firebase
 var config = {
@@ -10,7 +10,7 @@ var config = {
     projectId: "bookwatch-c8fbb",
     storageBucket: "bookwatch-c8fbb.appspot.com",
     messagingSenderId: "894052480603"
-    };
+};
 
 firebase.initializeApp(config);
 
@@ -23,11 +23,54 @@ let ref = database.ref('searchQuery');
 firebase.database().ref("searchQuery").on("value", dataChanged);
 
 //interate through the objects and push each search term into an array
-function dataChanged(data){
+function dataChanged(data) {
     let obj = data.val();
 
-    for (let key in obj){   // use for..in to interate through object keys
+    for (let key in obj) { // use for..in to interate through object keys
         let row = obj[key];
+        row = row.toLowerCase();
         allQueries.push(`${row}`);
-    }	
+    }
+
+    getCount(allQueries);
+
 }
+
+//iterates through searches and adds a counter to all of the searches to find which ones appear multiple times
+function getCount(original) {
+    for (let search of original) {
+        if (counts[search] == undefined) {
+            counts[search] = 1;
+        } else {
+            counts[search]++;
+        }
+    }
+
+    filteredSearches = Object.keys(counts); //array of all of the searches (no repeats)
+
+    console.log(filteredSearches);
+    for (let k of filteredSearches) {
+        console.log(`${k} : ${counts[k]}`);
+    }
+}
+
+//figure out how to make array of objects that consist of the key and the value
+
+/*let app = new Vue({
+    el: '#root',
+    data: {
+
+        filteredResults: filteredSearches,
+        searches: [],
+        searchPairs: {
+
+        }
+
+    },
+    methods: {
+        addSearches(){
+
+        }
+
+    }
+}); */
