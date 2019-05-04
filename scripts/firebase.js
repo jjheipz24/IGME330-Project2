d7d6549d9641e3f927525bf8e511851d51df40f5
jjheipz24 <heipler.jj@gmail.com>
2019-05-04 18:17:11 -0400

    loaded data into vue element

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,4 +1,4 @@
-let allQueries = []; //array to hold all searches
+/*let allQueries = []; //array to hold all searches
 let counts = {}; //object to add searches and counter
 let filteredSearches = []; //array of search results without repeats
 let numSearches = [] //array to hold the amount each search appeared
@@ -25,61 +25,107 @@ let ref = database.ref('searchQuery');
 
 firebase.database().ref("searchQuery").on("value", dataChanged);
 
+function dataChanged(data) {
+    obj = data.val();
+
+    for (let key in obj) { // use for..in to interate through object keys
+        let row = obj[key];
+        row = row.toLowerCase();
+        allQueries.push(`${row}`);
+    }
+
+    getCount(allQueries);
+
+}
+
+//iterates through searches and adds a counter to all of the searches to find which ones appear multiple times
+function getCount(original) {
+    for (let search of original) {
+        if (counts[search] == undefined) {
+            counts[search] = 1;
+        } else {
+            counts[search]++;
+        }
+    }
+
+    filteredSearches = Object.keys(counts); //array of all of the searches (no repeats)
+
+    console.log(filteredSearches);
+    for (let k of filteredSearches) {
+        console.log(`${k} : ${counts[k]}`);
+        numSearches.push(counts[k]);
+    }
+
+    console.log(numSearches);
+} */
+
 
 
 //figure out how to make array of objects that consist of the key and the value
-
 let app = new Vue({
-    el: '#root',
+    el: '#admin',
     data: {
-
-        filteredResults: ' ',
-        timesSearched: ' ',
         allQueries: [], //array to hold all searches
         counts: {}, //object to add searches and counter
         filteredSearches: [], //array of search results without repeats
         numSearches: [], //array to hold the amount each search appeared
-        obj: {}
-
+        // Initialize Firebase
+        config: {
+            apiKey: "AIzaSyB9rzeXc4ZlRJDIAXSJfxYp4ObIPRLawa8",
+            authDomain: "bookwatch-c8fbb.firebaseapp.com",
+            databaseURL: "https://bookwatch-c8fbb.firebaseio.com",
+            projectId: "bookwatch-c8fbb",
+            storageBucket: "bookwatch-c8fbb.appspot.com",
+            messagingSenderId: "894052480603"
+        },
+        database: '',
+        ref: '',
+        obj: '',
+        row: ''
+    },
+    mounted() {
+        this.startUp();
     },
     methods: {
-        //interate through the objects and push each search term into an array
-        function dataChanged(data) {
-            obj = data.val();
+        startUp() {
+            firebase.initializeApp(this.config);
+            this.database = firebase.database();
+            this.ref = this.database.ref('searchQuery');
+            this.ref.on("value", this.dataChanged);
+        },
+        dataChanged(data) {
+            this.obj = data.val();
 
-            for (let key in obj) { // use for..in to interate through object keys
-                let row = obj[key];
-                row = row.toLowerCase();
-                this.allQueries.push(`${row}`);
+            for (let key in this.obj) { // use for..in to interate through object keys
+                this.row = this.obj[key];
+                this.row = this.row.toLowerCase();
+                this.allQueries.push(`${this.row}`);
             }
 
-            getCount(allQueries);
+            this.getCount(this.allQueries);
 
         },
-
         //iterates through searches and adds a counter to all of the searches to find which ones appear multiple times
-        function getCount(original) {
+        getCount(original) {
             for (let search of original) {
-                if (counts[search] == undefined) {
-                    counts[search] = 1;
+                if (this.counts[search] == undefined) {
+                    this.counts[search] = 1;
                 } else {
-                    counts[search]++;
+                    this.counts[search]++;
                 }
-            },
-
-            filteredSearches = Object.keys(counts); //array of all of the searches (no repeats)
-
-            console.log(filteredSearches);
-            for (let k of filteredSearches) {
-                console.log(`${k} : ${counts[k]}`);
-                numSearches.push(counts[k]);
             }
 
-            console.log(numSearches);
-        }
-    },
+            this.filteredSearches = Object.keys(this.counts); //array of all of the searches (no repeats)
 
-    beforeMount() {
-        this.addSearches();
+            console.log(this.filteredSearches);
+            for (let k of this.filteredSearches) {
+                console.log(`${k} : ${this.counts[k]}`);
+                this.numSearches.push(this.counts[k]);
+            }
+
+            console.log(this.numSearches);
+        }
+
     }
+
 });
